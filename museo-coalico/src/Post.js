633e63988bb22dc6e966b52db5094e7feb6cbd13
Gomba66 <danielalejo66@hotmail.com>
2021-07-10 18:53:17 -0500

    [Feature] Show Individual Posts in a page

--- museo-coalico/src/Post.js
+++ museo-coalico/src/Post.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { css } from "@emotion/css";
 import { useParams } from "react-router-dom";
 import { API, Storage } from "aws-amplify";
 import { getPost } from "./graphql/queries";
@@ -21,9 +20,9 @@ export function Post() {
       console.log(postData);
       const currentPost = postData.data.getTodo;
       const image = await Storage.get(currentPost.image);
-      console.log('====================================');
+      console.log("====================================");
       console.log(image);
-      console.log('====================================');
+      console.log("====================================");
 
       currentPost.image = image;
       updatePost(currentPost);
@@ -35,40 +34,56 @@ export function Post() {
   console.log("post: ", post);
   return (
     <>
-    {loading ? <h3>Loading</h3> :
-    <>
-    <h1>Texto de prueba en Post</h1>
-      <h1 className={titleStyle}>{post.name}</h1>
-      <h3 className={locationStyle}>{post.location}</h3>
-      <p className={locationStyle}>{post.description}</p>
-      <p className={locationStyle}>{post.year}</p>
-      <p className={locationStyle}>{post.creation}</p>
-      <p className={locationStyle}>{post.link}</p>
-      <p className={locationStyle}>{post.published}</p>
-      <p className={locationStyle}>{post.region}</p>
-      <p className={locationStyle}>{post.category}</p>
-      <p className={locationStyle}>{post.subcategory}</p>
-      <p className={locationStyle}>{post.file}</p>
-      {/* <img alt="post" src={post.image} className={imageStyle} /> */}
-      </>
-    }
-    </>
+      {loading ? null : (
+        <div id="body-post">
+          {/* <!--Marcos--> */}
+          <div>
+            <img
+              id="marco"
+              className="pt-1 d-none d-md-block"
+              src={"../assets/marcos/marco-general.png"}
+              alt="background marco"
+              height="100%"
+              width="100%"
+            />
+          </div>
+          <div>
+            <img
+              id="marco"
+              className="pt-1 d-md-none"
+              src={"../assets/marcos/marco-general-mobile.png"}
+              alt="background marco"
+              height="100%"
+              width="100%"
+            />
+          </div>
 
+          {/* <!--Mobile - Título y párrafo--> */}
+          <div id="encabezado-p" className="row position-absolute d-flex">
+            <div className="col-12 d-none d-sm-block d-md-none d-block d-sm-none">
+              <h1 className="titulo-post">{post.name}</h1>
+            </div>
+            <div className="col-12 d-none d-sm-block d-md-none d-block d-sm-none">
+              <div className="scroll-p">
+                <p className="p-post">{post.description}</p>
+              </div>
+            </div>
+          </div>
+
+          {/* <!--Desktop - Título y párrafo--> */}
+          <div id="encabezado-p" className="d-flex position-absolute row">
+            <div className="col-12 d-md-block d-none">
+              <h1 className="titulo-post ml-5">{post.name}</h1>
+            </div>
+            <div className="col-5 d-md-block d-none ml-5">
+              <div className="scroll-p">
+                <p className="p-post">{post.description}</p>
+              </div>
+            </div>
+            <div className="col-6 d-md-block d-none"></div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
-const titleStyle = css`
-  margin-bottom: 7px;
-`;
-
-const locationStyle = css`
-  color: #0070f3;
-  margin: 0;
-`;
-
-const imageStyle = css`
-  max-width: 500px;
-  @media (max-width: 500px) {
-    width: 100%;
-  }
-`;
