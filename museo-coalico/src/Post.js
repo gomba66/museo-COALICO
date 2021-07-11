e5b7a5dfd6a4e4024891a5cc6548235f17ea07ea
Gomba66 <danielalejo66@hotmail.com>
2021-07-11 11:57:31 -0500

    [Feature] Now you can see the file of the post

--- museo-coalico/src/Post.js
+++ museo-coalico/src/Post.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { API, Storage } from "aws-amplify";
 import { getPost } from "./graphql/queries";
+// import { PDFReader } from "reactjs-pdf-reader";
 
 export function Post() {
   const [loading, updateLoading] = useState(true);
@@ -11,6 +12,8 @@ export function Post() {
   useEffect(() => {
     fetchPost();
   }, []);
+  // Create Document Component
+
   async function fetchPost() {
     try {
       const postData = await API.graphql({
@@ -31,6 +34,127 @@ export function Post() {
       console.log("error: ", err);
     }
   }
+
+  const typeFile = (post) => {
+    let formatImages = ["png", "jpg", "jpeg", "bmp", "HEIF"];
+    let formatAudios = [
+      "3pg",
+      "aa",
+      "aac",
+      "aax",
+      "act",
+      "aiff",
+      "alac",
+      "ape",
+      "au",
+      "awb",
+      "dss",
+      "flac",
+      "gsm",
+      "iklax",
+      "ivs",
+      "m4a",
+      "m4b",
+      "m4p",
+      "mmf",
+      "mp3",
+      "mpc",
+      "msv",
+      "nmf",
+      "oga",
+      "mogg",
+      "opus",
+      "ra",
+      "rm",
+      "raw",
+      "rf64",
+      "sln",
+      "tta",
+      "voc",
+      "vox",
+      "wav",
+      "wma",
+      "wv",
+      "8svx",
+      "cda",
+    ];
+    let formatVideos = ["flv", "ogg", "avi", "mov", "wmv", "mp4", "m4v"];
+    let formatDocs = ["pdf"];
+
+    let format = returnFormat(post);
+    let frame = "";
+    let url = post.file;
+    url = url.replace(/['"]+/g, "");
+
+    if (formatImages.includes(format)) {
+      frame = (
+        <>
+          <img
+            className="position-absolute"
+            src={`https://coalico-images101949-dev.s3.us-east-2.amazonaws.com/public/${url}`}
+            width="350px"
+            alt="Imagen"
+          />
+        </>
+      );
+    } else if (formatAudios.includes(format)) {
+      frame = (
+        <>
+          <audio>
+            <source
+              src={`https://coalico-images101949-dev.s3.us-east-2.amazonaws.com/public/${url}`}
+            />
+          </audio>
+        </>
+      );
+    } else if (formatImages.includes(format)) {
+      frame = (
+        <img
+          id="marco-slides"
+          src={"assets/marcos/texto-preview.png"}
+          alt="marco-slide"
+        />
+      );
+    } else if (formatVideos.includes(format)) {
+      frame = (
+        <>
+          <h3 style={{ color: "white" }}>File:</h3>
+          <p style={{ color: "white" }}>{format}</p>
+        </>
+      );
+    } else if (formatDocs.includes(format)) {
+      frame = (
+        <>
+          {/* <div style={{ overflow: "scroll", height: 600 }}>
+            <PDFReader
+              src={`https://coalico-images101949-dev.s3.us-east-2.amazonaws.com/public/${url}`}
+            />
+          </div> */}
+          <h3 style={{ color: "white" }}>File:</h3> {format}
+        </>
+      );
+    } else {
+      frame = (
+        <>
+          <h3 style={{ color: "white" }}>File:</h3> {format}
+        </>
+      );
+    }
+    return frame;
+  };
+
+  const returnFormat = (post) => {
+    if (post.file == null) {
+      return "";
+    }
+    let divided = post.file.split(".");
+    let format = divided[divided.length - 1];
+    format = format.replace('"', "");
+    if (format.includes("[")) {
+      format = null;
+    }
+    return format;
+  };
   console.log("post: ", post);
   return (
     <>
@@ -80,7 +204,7 @@ export function Post() {
                 <p className="p-post">{post.description}</p>
               </div>
             </div>
-            <div className="col-6 d-md-block d-none"></div>
+            <div className="col-6 d-md-block d-none">{typeFile(post)}</div>
           </div>
         </div>
       )}
