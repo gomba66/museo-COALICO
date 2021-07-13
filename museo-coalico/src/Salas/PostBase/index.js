1796578ba4f408a4de275333c63bb805360d5db2
nickmoran06 <nicolasmoranpulido@gmail.com>
2021-07-13 14:01:55 -0500

    [Feature] Style of images and PDF

--- museo-coalico/src/Salas/PostBase/index.js
+++ museo-coalico/src/Salas/PostBase/index.js
@@ -41,7 +41,7 @@ const PostBase = (props) => {
                     <h1 className="titulo-post">Heridas del conflicto</h1>
                 </div>
                 <div className="col-12 d-none d-sm-block d-md-none d-block d-sm-none">
-                <div className="scroll-p">
+                    <div className="scroll-p">
                         <p className="p-post">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
 
